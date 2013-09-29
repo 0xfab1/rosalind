497b8108ea592c24e279539907a9a01681db9dde
Fabianus Mulawadi <fabianus@gmail.com>
2013-09-29 14:48:56 +0800

    mrna

---
+++ src/mrna.py
@@ -0,0 +1,31 @@
+__author__ = 'Fabianus'
+
+aa = {'*': 3,
+      'A': 4,
+      'C': 2,
+      'D': 2,
+      'E': 2,
+      'F': 2,
+      'G': 4,
+      'H': 2,
+      'I': 3,
+      'K': 2,
+      'L': 6,
+      'M': 1,
+      'N': 2,
+      'P': 4,
+      'Q': 2,
+      'R': 6,
+      'S': 6,
+      'T': 4,
+      'V': 4,
+      'W': 1,
+      'Y': 2}
+
+import sys
+
+val = aa['*']
+for b in sys.stdin.read().strip():
+    val = (val * aa[b]) % 1000000
+
+print val
