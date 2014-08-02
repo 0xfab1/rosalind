e89b82d1a6813cfe627f7d2ce4206e9042d53e08
Fabianus Mulawadi <fabianus@gmail.com>
2014-08-03 00:46:39 +0800

    Solved Mortal Fibonacci Rabbits

---
+++ src/fibd.py
@@ -0,0 +1,18 @@
+__author__ = 'fabianus'
+
+import numpy as np
+
+n = 94  # the question is how many rabbits in month n
+m = 20  # rabbit lifespan (months)
+k = 1  # how many pairs of children born to every reproductive pair
+
+wabbits = np.zeros(n, dtype='object')  # use python number to avoid integer overflow
+wabbits[0] = 1  # start with 1 pair of rabbit in month 1
+
+rates = np.zeros((n, n), dtype='object')
+for i in xrange(m - 1):
+    rates[i, i + 1] = 1  # rabbits do not survive beyond m month
+
+rates[1:n, 0] = k  # after 2 months, rabbits start reproducing
+
+print np.sum(wabbits.dot(np.linalg.matrix_power(rates, n - 1)))
