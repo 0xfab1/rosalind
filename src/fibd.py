b5ebefa10806320abb213a2c2cf659a2d6607aa2
Fabianus Mulawadi <fabianus@gmail.com>
2014-08-03 09:52:13 +0800

    There's no need to use n x n matrix if the rabbits die after m months.

--- src/fibd.py
+++ src/fibd.py
@@ -6,13 +6,13 @@ n = 94  # the question is how many rabbits in month n
 m = 20  # rabbit lifespan (months)
 k = 1  # how many pairs of children born to every reproductive pair
 
-wabbits = np.zeros(n, dtype='object')  # use python number to avoid integer overflow
+wabbits = np.zeros(m, dtype='object')  # use python number to avoid integer overflow
 wabbits[0] = 1  # start with 1 pair of rabbit in month 1
 
-rates = np.zeros((n, n), dtype='object')
+rates = np.zeros((m, m), dtype='object')
 for i in xrange(m - 1):
     rates[i, i + 1] = 1  # rabbits do not survive beyond m month
 
-rates[1:n, 0] = k  # after 2 months, rabbits start reproducing
+rates[1:, 0] = k  # after 2 months, rabbits start reproducing
 
 print np.sum(wabbits.dot(np.linalg.matrix_power(rates, n - 1)))
